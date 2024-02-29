093dabeb34a1651c02be9a13c5aa5764954719b2
Engelsgeduld <knyazev.dmitrii05@gmail.com>
2024-02-29 23:15:44 +0300

    Init: Homework 1 Task 1 Registry Knyazev

---
+++ src/homeworks/homework_1/registry.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+
+class Registry:
+    def __getitem__(self, item: type):
+        self.main_class = item
+        return self
+
+    def __init__(self, default: type = None) -> None:
+        self.default = default
+        self.register_of_names: dict[str, type] = {}
+
+    def register(self, name: str) -> Callable:
+        def decorator(cls: type) -> type:
+            if not issubclass(cls, self.main_class):
+                raise Exception(f"{cls.__name__} is not a subclass of {self.main_class.__name__}")
+            self.register_of_names[name] = cls
+            return cls
+
+        return decorator
+
+    def dispatch(self, name: str) -> type:
+        if name in self.register_of_names:
+            return self.register_of_names[name]
+        if self.default is None:
+            raise Exception(f"{name} realisation not registered")
+        return self.default
---
+++ tests/test_homework_1/test_registry.py
@@ -0,0 +1,64 @@
+from typing import Mapping
+
+from src.homeworks.homework_1.registry import Registry
+import pytest
+
+
+MAPPING_REGISTER_1 = Registry(default=dict)[Mapping]
+OBJECT_REGISTER_1 = Registry()[object]
+
+
+@MAPPING_REGISTER_1.register(name="mapping")
+class TestClassMapping(Mapping):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+
+class TestMappingSubclass(TestClassMapping):
+    pass
+
+
+@OBJECT_REGISTER_1.register(name="object")
+class TestClassObj(object):
+    pass
+
+
+def test_registry_default():
+    assert isinstance(MAPPING_REGISTER_1.dispatch("obj")(), dict)
+
+
+def test_registry_dispatch():
+    assert isinstance(MAPPING_REGISTER_1.dispatch("mapping")(), TestClassMapping)
+
+
+def test_registry_wrong_parent_class():
+    with pytest.raises(Exception):
+        MAPPING_REGISTER_1.register(name="wrong")(TestClassObj)
+
+
+def test_registry_dispatch_exception():
+    with pytest.raises(Exception):
+        OBJECT_REGISTER_1.dispatch("wrong")
+
+
+def test_diff_registers():
+    mapping_register_two = Registry()[Mapping]
+    mapping_register_two.register(name = "dict")(dict)
+    assert getattr(mapping_register_two, "register_of_names") != getattr(MAPPING_REGISTER_1, "register_of_names")
+
+
+@pytest.mark.parametrize("name, cls", [("dict", dict), ("testmappingsubclass", TestMappingSubclass)])
+def test_registry_normal(name, cls):
+    MAPPING_REGISTER_1.register(name)(cls)
+    assert isinstance(MAPPING_REGISTER_1.dispatch(name)(), cls)
+
+
